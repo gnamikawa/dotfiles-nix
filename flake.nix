d0a9e9525efef35ad2f629c630d248be8c1c2a06
Genzo Namikawa <genzo.namikawa@outlook.com>
2026-03-20 22:24:31 +0900

    initial commit

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Home Manager configuration of genzo";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    homemanager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { nixpkgs, homemanager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      homeConfigurations."genzo" = homemanager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+    };
+}
