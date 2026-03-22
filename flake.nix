4e5c5d7a2b1fa58bd9eb0ae8056367eec6f5487c
Genzo Namikawa <genzo.namikawa@outlook.com>
2026-03-22 13:01:57 +0900

    chore: update comments

--- flake.nix
+++ flake.nix
@@ -37,17 +37,12 @@
       homeConfigurations."genzo" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
 
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
         modules = [
           ./home.nix
         ];
         extraSpecialArgs = {
           inherit pkgs2511;
         };
-
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
       };
     };
 }
