7fa72d99c399727aad803c0f4fb96fa029333953
Genzo Namikawa <genzo.namikawa@outlook.com>
2026-04-08 03:29:11 +0900

    fix: let home-manager manage fcitx5 and the input method
    Moved out of the system configuration as the active addons directory
    will be confusing/unexpected if both home-manager configurations and
    nixos configurations are active at the same time.
    
    DO NOT enable i18n in home-manager and nixOS at the same time.

--- home.nix
+++ home.nix
@@ -71,6 +71,36 @@
     };
   };
 
+  i18n = {
+    inputMethod = {
+      enable = true;
+      type = "fcitx5";
+      fcitx5 = {
+        addons = with pkgs; [
+          fcitx5-gtk
+          fcitx5-mozc-ut
+        ];
+        waylandFrontend = true;
+        settings.inputMethod = {
+          "Groups/0" = {
+            "Name" = "Default";
+            "Default Layout" = "keyboard-us";
+            "DefaultIM" = "keyboard-us";
+          };
+          "Groups/0/Items/0" = {
+            "Name" = "keyboard-us";
+          };
+          "Groups/0/Items/1" = {
+            "Name" = "mozc";
+          };
+          "GroupOrder" = {
+            "0" = "Default";
+          };
+        };
+      };
+    };
+  };
+
   systemd.user.services = {
     top-waybar = {
       Unit = {
