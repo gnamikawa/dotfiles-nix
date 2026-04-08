2383ab93eb9306442241f65cb3af24b1aeeecb57
Genzo Namikawa <genzo.namikawa@outlook.com>
2026-04-08 15:02:37 +0900

    feat: apply geist design system to mako

--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, constants, ... }:
 {
   imports = [
     ./programs/bash.nix
@@ -101,6 +101,56 @@
     };
   };
 
+  services.mako = {
+    enable = true;
+
+    settings = {
+      sort = "-time";
+      layer = "overlay";
+
+      width = 300;
+      height = 110;
+
+      border-size = 1;
+      border-radius = 4;
+
+      default-timeout = 5000;
+      ignore-timeout = true;
+
+      background-color = constants.theme.componentBackground.active.gray;
+      border-color = constants.theme.border.active.gray;
+      text-color = constants.theme.text.primary.gray;
+      font = "JetBrainsMono Nerd Font,JetBrainsMono NF 10";
+
+      max-icon-size = 64;
+      icons = true;
+
+      "urgency=low" = {
+        background-color = constants.theme.componentBackground.active.teal;
+        border-color = constants.theme.border.active.teal;
+        text-color = constants.theme.text.primary.teal;
+      };
+
+      "urgency=normal" = {
+        background-color = constants.theme.componentBackground.active.gray;
+        border-color = constants.theme.border.active.gray;
+        text-color = constants.theme.text.primary.gray;
+      };
+
+      "urgency=high" = {
+        background-color = constants.theme.componentBackground.active.red;
+        border-color = constants.theme.border.active.red;
+        text-color = constants.theme.text.primary.red;
+        default-timeout = 0;
+      };
+
+      "category=mpd" = {
+        default-timeout = 2000;
+        group-by = "category";
+      };
+    };
+  };
+
   systemd.user.services = {
     top-waybar = {
       Unit = {
@@ -153,19 +203,6 @@
       Service = {
         Type = "simple";
         ExecStart = "${pkgs.fcitx5}/bin/fcitx5";
-      };
-      Install.WantedBy = [ "graphical-session.target" ];
-    };
-
-    mako = {
-      Unit = {
-        Description = "Mako Notifications";
-        After = [ "graphical-session.target" ];
-        PartOf = [ "graphical-session.target" ];
-      };
-      Service = {
-        Type = "simple";
-        ExecStart = "${pkgs.mako}/bin/mako";
       };
       Install.WantedBy = [ "graphical-session.target" ];
     };
