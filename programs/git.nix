f62f0288d6938657ccaca32f69ee20119c6de363
Genzo Namikawa <genzo.namikawa@outlook.com>
2026-04-16 13:50:04 +0900

    feat: configure git rebase/autostash

--- programs/git.nix
+++ programs/git.nix
@@ -4,5 +4,8 @@
     enable = true;
     settings.user.name = "Genzo Namikawa";
     settings.user.email = "genzo.namikawa@outlook.com";
+    settings.pull.rebase = true;
+    settings.pull.autoStash = true;
+    settings.rebase.autoStash = true;
   };
 }
