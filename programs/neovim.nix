e41a5db8232401d80cebae8c5b06a627f072ac4b
Genzo Namikawa <genzo.namikawa@outlook.com>
2026-04-18 08:13:56 +0900

    fix: do not enable neovim configuration
    allow neovim to be managed via config file

--- programs/neovim.nix
+++ programs/neovim.nix
@@ -1,32 +1,33 @@
 { pkgs, ... }:
 {
-  programs.neovim = {
-    enable = true;
-    withPython3 = true;
-    withRuby = true;
-    withNodeJs = true;
-    defaultEditor = true;
-    extraPackages = with pkgs; [
-      go
-      cargo
-      clang
-      nodejs_24
-      unzip
-      luarocks
-      fzf
-      ripgrep
-      ghostscript
-      mermaid-cli
-      tectonic
-      tetex
-      kitty
-      wezterm
-      ghostty
-      fd
-      lua5_1
-      sqlite
-      lazygit
-      tree-sitter
-    ];
+  home.sessionVariables = {
+    EDITOR = "nvim";
   };
+  home.packages = with pkgs; [
+    (neovim.override {
+      withPython3 = true;
+      withRuby = true;
+      withNodeJs = true;
+    })
+    go
+    cargo
+    clang
+    nodejs_24
+    unzip
+    luarocks
+    fzf
+    ripgrep
+    ghostscript
+    mermaid-cli
+    tectonic
+    tetex
+    kitty
+    wezterm
+    ghostty
+    fd
+    lua5_1
+    sqlite
+    lazygit
+    tree-sitter
+  ];
 }
