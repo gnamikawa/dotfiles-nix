325cdb9e3fa77eadd184c5526c44b3875d3e12ab
Genzo Namikawa <genzo.namikawa@outlook.com>
2026-04-18 10:14:38 +0900

    refactor: make easier to read

--- home.nix
+++ home.nix
@@ -173,32 +173,24 @@
     autostart.enable = true;
   };
 
-  i18n = {
-    inputMethod = {
-      enable = true;
-      type = "fcitx5";
-      fcitx5 = {
-        addons = with pkgs; [
-          fcitx5-gtk
-          fcitx5-mozc-ut
-        ];
-        waylandFrontend = true;
-        settings.inputMethod = {
-          "Groups/0" = {
-            "Name" = "Default";
-            "Default Layout" = "keyboard-us";
-            "DefaultIM" = "keyboard-us";
-          };
-          "Groups/0/Items/0" = {
-            "Name" = "keyboard-us";
-          };
-          "Groups/0/Items/1" = {
-            "Name" = "mozc";
-          };
-          "GroupOrder" = {
-            "0" = "Default";
-          };
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5 = {
+      addons = with pkgs; [
+        fcitx5-gtk
+        fcitx5-mozc-ut
+      ];
+      waylandFrontend = true;
+      settings.inputMethod = {
+        "Groups/0" = {
+          "Name" = "Default";
+          "Default Layout" = "keyboard-us";
+          "DefaultIM" = "keyboard-us";
         };
+        "Groups/0/Items/0".Name = "keyboard-us";
+        "Groups/0/Items/1".Name = "mozc";
+        "GroupOrder"."0" = "Default";
       };
     };
   };
