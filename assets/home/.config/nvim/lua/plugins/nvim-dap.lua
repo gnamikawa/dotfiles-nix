6ba07e414509387cc255f99b3a36da2e66b2212c
Genzo Namikawa <genzo.namikawa@outlook.com>
2026-05-08 19:44:15 +0900

    feat: customise behavior of nvim dap-ui to prevent the ui from exiting upon finish

---
+++ assets/home/.config/nvim/lua/plugins/nvim-dap.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = { "nvim-neotest/nvim-nio" },
+    keys = {
+      {
+        "<leader>du",
+        function()
+          require("dapui").toggle({})
+        end,
+        desc = "Dap UI",
+      },
+      {
+        "<leader>de",
+        function()
+          require("dapui").eval()
+        end,
+        desc = "Eval",
+        mode = { "n", "x" },
+      },
+    },
+    opts = {},
+    config = function(_, opts)
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup(opts)
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open({})
+      end
+    end,
+  },
+}
